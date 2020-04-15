cb297faac7bfa4369ab5356d54961c77e0b4a463
Viktor Lovgren <lovgren.viktor@gmail.com>
2020-04-15 12:57:34 +0700

    adding -- to h2 in text

--- js/meditationList.js
+++ js/meditationList.js
@@ -68,22 +68,22 @@ function printDay(selectedDay){
   meditation=meditations[selectedDay];
   message=`<h2>Day ${meditation.day}</h2>`
   if( meditation.introduction != ''){
-  message+=`<h3>Introduction</h3>${meditation.introduction}`;
+  message+=`<h3>--Introduction--</h3>${meditation.introduction}`;
   }
   if( meditation.task != ''){
-  message+=`<h3>Task</h3>${meditation.task}`;
+  message+=`<h3>--Task--</h3>${meditation.task}`;
   }
   if( meditation.phrase != ''){
-  message+=`<h3>Phrase</h3>${meditation.phrase}`;
+  message+=`<h3>--Phrase--</h3>${meditation.phrase}`;
   }
   if( meditation.meditation != ''){
-  message+=`<h3>Meditation</h3>${meditation.meditation}`;
+  message+=`<h3>--Meditation--</h3>${meditation.meditation}`;
   }
   if( meditation.meditationURL != ''){
-  message+=`<h3>Link to  meditation</h3>${meditation.meditationURL}`;
+  message+=`<h3>--Link to  meditation--</h3>${meditation.meditationURL}`;
   }
   if( meditation.recap != ''){
-  message+=`<h3>Recap for day ${meditation.day}</h3>${meditation.recap}`;
+  message+=`<h3>--Recap for day ${meditation.day}--</h3>${meditation.recap}`;
   }
   message+=`<p>Have a great day!</p>`
   if(leaderName!=''){
@@ -98,22 +98,22 @@ for(let i=0;i<meditations.length;i++){
     meditation=meditations[i];
     message+=`<h2>Day ${meditation.day}</h2>`
     if( meditation.introduction != ''){
-    message+=`<h3>Introduction</h3>${meditation.introduction}`;
+    message+=`--<h3>Introduction--</h3>${meditation.introduction}`;
     }
     if( meditation.task != ''){
-    message+=`<h3>Task</h3>${meditation.task}`;
+    message+=`<h3>--Task--</h3>${meditation.task}`;
     }
     if( meditation.phrase != ''){
-    message+=`<h3>Phrase</h3>${meditation.phrase}`;
+    message+=`<h3>--Phrase--</h3>${meditation.phrase}`;
     }
     if( meditation.meditation != ''){
-    message+=`<h3>Meditation</h3>${meditation.meditation}`;
+    message+=`<h3>--Meditation--</h3>${meditation.meditation}`;
     }
     if( meditation.meditationURL != ''){
-    message+=`<h3>Link to  meditation</h3>${meditation.meditationURL}`;
+    message+=`<h3>--Link to  meditation--</h3>${meditation.meditationURL}`;
     }
     if( meditation.recap != ''){
-    message+=`<h3>Recap for day ${meditation.day}</h3>${meditation.recap}`;
+    message+=`<h3>--Recap for day ${meditation.day}--</h3>${meditation.recap}`;
     }
     message+=`<p>Have a great day!</p>`
     if(leaderName!=''){
